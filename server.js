b41921410e4b62728f821cacbb615d92d5b029c3
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-02-01 19:23:57 -0500

    Added random nice comments on reload

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var http = require("http");
 
-
+var nice = [" You are awesome", "You are a great friend", "I can always count on you"]
 
 var nicePORT = 7000;
 var handlerRequestNice = function (req, res){
-  res.end ("You're Awesome");
+  var num = Math.floor(Math.random()*nice.length);
+  res.end (nice[num]);
 };
 var niceServer = http.createServer(handlerRequestNice);
 
@@ -13,6 +14,7 @@ niceServer.listen(nicePORT, function(){
 });
 
 
+var mean = [" You are terrible", "You are a paper friend", "You are never there for me"]
 
 
 var meanPORT = 7500;
